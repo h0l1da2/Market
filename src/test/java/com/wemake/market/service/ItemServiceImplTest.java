fdf474e4b29f2a79cbf06f08fad366ddb79502fe
h0l1da2 <whaleee@naver.com>
2023-09-01 14:53:09 +0900

    test: 아이템 추가 실패 케이스
    중복 아이템/유저 요청 실패 케이스 추가(성공)

--- src/test/java/com/wemake/market/service/ItemServiceImplTest.java
+++ src/test/java/com/wemake/market/service/ItemServiceImplTest.java
@@ -1,10 +1,12 @@
 package com.wemake.market.service;
 
+import com.wemake.market.domain.Item;
 import com.wemake.market.domain.Role;
 import com.wemake.market.domain.dto.ItemDto;
 import com.wemake.market.exception.ItemDuplException;
 import com.wemake.market.exception.NotAuthorityException;
 import com.wemake.market.repository.ItemRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -41,4 +43,30 @@ class ItemServiceImplTest {
         assertThat(item.getName()).isEqualTo(itemDto.getName());
         assertThat(item.getPrice()).isEqualTo(itemDto.getPrice());
     }
+
+    @Test
+    @DisplayName("아이템 추가 실패 : 유저 요청")
+    void 아이템추가_실패_유저요청() {
+        // given
+        ItemDto itemDto = new ItemDto("name", 1000, Role.USER);
+
+        // when then
+        Assertions.assertThrows(NotAuthorityException.class,
+                () -> itemService.createItem(itemDto));
+
+    }
+
+    @Test
+    @DisplayName("아이템 추가 실패 : 중복 아이템)")
+    void 아이템추가_실패_중복아이템() {
+        // given
+        ItemDto itemDto = new ItemDto("name", 1000, Role.MARKET);
+        itemRepository.save(new Item(itemDto, false));
+
+        // when then
+        Assertions.assertThrows(ItemDuplException.class,
+                () -> itemService.createItem(itemDto));
+
+    }
+
 }
