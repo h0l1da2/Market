2f30173088be3944b4b0a66ee4a4877ed392d116
h0l1da2 <whaleee@naver.com>
2023-09-05 20:09:21 +0900

    feat: order 추가
    오더 엔티티 추가

---
+++ src/main/java/com/wemake/market/domain/Order.java
@@ -0,0 +1,20 @@
+package com.wemake.market.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+    private int count;
+    private int deliveryPrice;
+
+}
