662fde131055f270487a200dea93438858eda091
h0l1da2 <whaleee@naver.com>
2023-09-08 10:17:15 +0900

    feat: 양식 에러 관련 핸들러 추가
    FormError 핸들러 추가

--- src/main/java/com/wemake/market/handler/ExceptionController.java
+++ src/main/java/com/wemake/market/handler/ExceptionController.java
@@ -1,5 +1,6 @@
 package com.wemake.market.handler;
 
+import com.wemake.market.exception.FormErrorException;
 import com.wemake.market.exception.NotAuthorityException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,12 @@ public class ExceptionController {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(NotAuthorityException.class)
     public ResponseEntity<String> haveNotMarketAuthExceptionHandler(NotAuthorityException e) {
+        return ResponseEntity.badRequest()
+                .body(e.getErrorMessage());
+    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(FormErrorException.class)
+    public ResponseEntity<String> formErrorExceptionHandler(FormErrorException e) {
         return ResponseEntity.badRequest()
                 .body(e.getErrorMessage());
     }
